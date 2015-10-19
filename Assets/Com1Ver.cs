e8244d5789c2a073fefe52e9fc07c115a96eae1d
CrazyQ <legendcyqkt@qq.com>
2015-10-19 11:19:37 +0800

    初始建立

---
+++ Assets/Com1Ver.cs
@@ -0,0 +1,20 @@
+﻿//--------------------------------
+// 		GreatMedia 
+// 创建者:CrazyQ 2015
+// 修改者:
+// 描述:
+//--------------------------------
+
+using UnityEngine;
+using System.Collections;
+
+public class Com1Ver : MonoBehaviour
+{
+
+	void Start () 
+	{
+	
+	}
+	
+
+}
